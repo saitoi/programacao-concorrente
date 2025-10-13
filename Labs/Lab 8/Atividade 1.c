2b0d2756417f231fa0d6c9161b9153707a183ac2
saitoi <pedrohhs@dcc.ufrj.br>
2025-10-13 19:43:30 -0300

    fix: concurrency bugs + update: Makefile

--- Labs/Lab 8/Atividade 1.c
+++ Labs/Lab 8/Atividade 1.c
@@ -11,14 +11,30 @@
 // Várias threads consumidoras avaliam a primalidade e tem contagem individual
 // Thread produtora preenche buffer de vez
 
+// Observações
+// Por padrão, os números gerados pela thread produtora são aleatórios.
+// No entanto, o usuário poderá passar um intervalo.
+
 #define MAX 1000
 #define NTHREADS_CONS 4
 
+#ifdef VERBOSE
+#define LOG(fmt, ...) fprintf(stderr, "[VERBOSE] " fmt, ##__VA_ARGS__)
+#else
+#define LOG(fmt, ...) ((void)0)
+#endif
+
+
 typedef struct {
   int *buffer; // Buffer de tamanho M
-  int id, N, M;
+  int id,
+      N,
+      M,
+      inicio,
+      qtd_buffer;
 } thread_args;
 
+int qtd_buffer = 0;
 sem_t mutex, vazio, cheio;
 
 static inline int ehPrimo(long long int n) {
@@ -33,6 +49,19 @@ static inline int ehPrimo(long long int n) {
     if (n % i == 0)
       return 0;
   return 1;
+}
+
+/* Funções para gerar entradas sequenciais ou aleatórias */
+
+int *gerar_entradas_seq(int inicio, int dim) {
+    int *arr = (int *)malloc(sizeof(int) * dim);
+    if (!arr) {
+      fprintf(stderr, "Erro durante a alocação do vetor de int sequencial.\n");
+      pthread_exit(NULL);
+    }
+    for (int i = 0; i < dim; ++i)
+        arr[i] = i + inicio;
+    return arr;
 }
 
 int *gerar_entradas_aleat(int dim) {
@@ -51,10 +80,11 @@ int retirar(int *buffer, int dim) {
   int aux;
   sem_wait(&cheio);
   sem_wait(&mutex);
-  aux = buffer[in++];
-  if (in == dim)
+  aux = buffer[in];
+  in = (in + 1) % dim;
+  qtd_buffer--;
+  if (qtd_buffer == 0)
     sem_post(&vazio);
-  in %= dim;
   sem_post(&mutex);
   return aux;
 }
@@ -64,20 +94,26 @@ void inserir(int *buffer, int *entradas, int dim) {
   sem_wait(&mutex);
   for (int i = 0; i < dim; ++i) {
     buffer[i] = entradas[i];
-    sem_post(&cheio);
+    qtd_buffer++;
   }
   sem_post(&mutex);
+  for (int i = 0; i < dim; ++i) {
+    sem_post(&cheio);
+  }
 }
 
 void *produtora(void *args) {
   thread_args *t = (thread_args *)args;
-  int *entradas = gerar_entradas_aleat(t->N);
-  puts("Entrada aleatoria gerada: ");
+  int *entradas = (t->inicio > -1) ? gerar_entradas_seq(t->inicio, t->N) : gerar_entradas_aleat(t->N);
+#ifdef VERBOSE
+  fprintf(stderr, "[VERBOSE] Entrada aleatoria gerada: ");
   for (int i = 0; i < t->N; i++)
-    printf("%d ", entradas[i]);
-  puts("\n");
+    fprintf(stderr, "%d ", entradas[i]);
+  fprintf(stderr, "\n");
+#endif
   for (int i = 0; i < t->N; i += t->M) {
-    inserir(t->buffer, &entradas[i], t->M);
+    int qtd_inserir = ((t->N - i) < t->M) ? (t->N - i) : t->M;
+    inserir(t->buffer, &entradas[i], qtd_inserir);
   }
   pthread_exit((void*) entradas);
 }
@@ -87,12 +123,12 @@ void *consumidora(void *args) {
   int *qtd_primos = (int *)malloc(sizeof(int));
   if (!qtd_primos) {
     fprintf(stderr, "Erro durante alocação da variável qtd_primos.\n");
-    free(qtd_primos);
     pthread_exit(NULL);
   }
+  *qtd_primos = 0;
 
   int rem = t->N % NTHREADS_CONS;
-  int chunk = (t->N / NTHREADS_CONS) + ((t->id < rem) ? t->id : rem);
+  int chunk = (t->N / NTHREADS_CONS) + ((t->id <= rem) ? 1 : 0);
 
   for (int i = 0; i < chunk; i++) {
     int aux = retirar(t->buffer, t->M);
@@ -106,18 +142,20 @@ int main(int argc, char *argv[]) {
   srand(time(NULL));
   pthread_t *tids;
   int N, // Quantidade de inteiros aleatorios gerados (int)
-      M; // Tamanho do canal (int)
+      M, // Tamanho do canal (int)
+      inicio; // Parâmetro opcional para gerar um intervalo sequencial
   int qtd_primos_total = 0, qtd_primos_seq = 0, qtd_primos_vencedora = 0, vencedora = 0;
   int *entradas;
 
   if (argc < 3) {
-    fprintf(stderr, "Uso: %s <qtd. de inteiros gerados> <tamanho do canal>\n",
+    fprintf(stderr, "Uso: %s <qtd. de inteiros gerados> <tamanho do canal> <inicio>\n",
             argv[0]);
     return 1;
   }
 
   N = (int)atoi(argv[1]);
   M = (int)atoi(argv[2]);
+  inicio = (int)((argc == 4) ? atoi(argv[3]) : -1);
 
   if (N < 1 || M < 1 || M >= N) {
     fprintf(stderr, "Entrada de qtd. inteiros gerados ou o tamanho do canal "
@@ -127,6 +165,11 @@ int main(int argc, char *argv[]) {
                     "qtd. de inteiros.\n");
     return 1;
   }
+
+  LOG("Entrada:\n"
+         "- N: %d\n"
+         "- M: %d\n"
+         "- inicio: %d\n", N, M, inicio);
 
   tids = (pthread_t *)malloc(sizeof(pthread_t) * (1 + NTHREADS_CONS));
   if (!tids) {
@@ -147,13 +190,14 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  arg->buffer = buffer;
-  arg->N = N;
-  arg->M = M;
-
   sem_init(&mutex, 0, 1);
   sem_init(&vazio, 0, 1);
   sem_init(&cheio, 0, 0);
+
+  arg->inicio = inicio;
+  arg->buffer = buffer;
+  arg->N = N;
+  arg->M = M;
 
   // Primeira thread produtora
   if (pthread_create(&tids[0], NULL, produtora, (void *)arg) != 0) {
@@ -162,15 +206,25 @@ int main(int argc, char *argv[]) {
   }
 
   // Restante das threads consumidoras
+  thread_args *args_cons[NTHREADS_CONS];
   for (int i = 1; i < NTHREADS_CONS + 1; ++i) {
-    arg->id = i;
-    if (pthread_create(&tids[i], NULL, consumidora, (void *)arg) != 0) {
+    args_cons[i-1] = (thread_args *)malloc(sizeof(thread_args));
+    if (!args_cons[i-1]) {
+      fprintf(stderr, "Erro durante a alocação da estrutura da thread consumidora %d.\n", i);
+      return 1;
+    }
+    args_cons[i-1]->id = i;
+    args_cons[i-1]->buffer = buffer;
+    args_cons[i-1]->N = N;
+    args_cons[i-1]->M = M;
+    if (pthread_create(&tids[i], NULL, consumidora, (void *)args_cons[i-1]) != 0) {
       fprintf(stderr, "Erro durante a criação das threads consumidoras.\n");
       return 1;
     }
   }
 
   // Join da thread produtora
+  // Variável entradas para capturar o vetor gerado pela produtora
   if (pthread_join(tids[0], (void**) &entradas) != 0) {
     fprintf(stderr, "Erro na junção da thread produtora.\n");
     return 1;
@@ -188,6 +242,8 @@ int main(int argc, char *argv[]) {
       vencedora = i;
       qtd_primos_vencedora = *qtd_primos;
     }
+    free(qtd_primos);
+    free(args_cons[i-1]);
   }
 
   sem_destroy(&mutex);
@@ -203,6 +259,12 @@ int main(int argc, char *argv[]) {
   printf("Quantidade total de primos (seq): %d\n", qtd_primos_seq);
   printf("Thread %d foi a vencedora com %d primos.\n", vencedora,
          qtd_primos_vencedora);
+
+  // Liberar memória
+  free(entradas);
+  free(buffer);
+  free(arg);
+  free(tids);
 
   return 0;
 }
